a83af1dd19ebce8fef559fe80b57d33998aef3c4
Mikron93 <mhapgood93@hotmail.co.uk>
2021-11-16 09:50:19 +0000

    Activities created and named

--- app/src/main/java/com/example/comp2000/PaymentComplete.java
+++ app/src/main/java/com/example/comp2000/PaymentComplete.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PaymentComplete extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_payment_complete);
     }
 }
